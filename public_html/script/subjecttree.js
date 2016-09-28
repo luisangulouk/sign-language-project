2b3b805ddf89fc87c2836765eb5a17d74b377883
Luis Miguel <Luismi@Robs-iMac-3.local>
2016-09-28 17:24:14 +0100

    Adding the rest of the nodes for the information tree

--- public_html/script/subjecttree.js
+++ public_html/script/subjecttree.js
@@ -1,4 +1,8 @@
 category={
+	"Adjectives" : [{ "subject":"What are adjectives?","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Comparative Adjectives","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Superlative Adjectives","video":"Information Video","article":"Information sheet","worksheets":"worksheets"}
+				],	
 	"adverbs" : [{ "subject":"What are adverbs?","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
 				{ "subject":"Adverbs of frequency","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
 				{ "subject":"Adverbs of manner","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
@@ -6,12 +10,72 @@ category={
 				{ "subject":"Adverbs of time","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
 				{ "subject":"Adverbs of degree","video":"Information Video","article":"Information sheet","worksheets":"worksheets"}
 				],	
-	"adverbs2" : [{ "subject":"What are adverbs?","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
-				{ "subject":"Adverbs of frequency","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
-				{ "subject":"Adverbs of manner","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
-				{ "subject":"Adverbs of place","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
-				{ "subject":"Adverbs of time","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
-				{ "subject":"Adverbs of degree","video":"Information Video","article":"Information sheet","worksheets":"worksheets"}
-				]	
-
+	"Affixes" : [{ "subject":"What are affixes?","video":"Information Video","article":"Information sheet","worksheets":"worksheets"}
+				],
+	"Conditionals -if clauses-" : [{ "subject":"What are conditionals","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Zero Conditional","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"1st Conditional","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"2nd Conditional","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"3rd Conditional","video":"Information Video","article":"Information sheet","worksheets":"worksheets"}
+				],
+	"Contractions" : [{ "subject":"What are contractions?","video":"Information Video","article":"Information sheet","worksheets":"worksheets"}
+				],
+	"Demonstratives" : [{ "subject":"What are demonstratives?","video":"Information Video","article":"Information sheet","worksheets":"worksheets"}
+				],
+	"Idioms" : [{ "subject":"What are idioms?","video":"Information Video","article":"Information sheet","worksheets":"worksheets"}
+				],
+	"Meta Language" : [{ "subject":"What is meta language","video":"Information Video","article":"Information sheet","worksheets":"worksheets"}
+				],
+	"Nouns" : [{ "subject":"What are nouns","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Proper Nouns","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Abstract and concrete nouns","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Count Nouns and non-count nouns","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Changing nouns from Singular to plural","video":"Information Video","article":"Information sheet","worksheets":"worksheets"}
+				],
+	"Numbers and dates" : [{ "subject":"Ordinal Numbers","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Writing dates","video":"Information Video","article":"Information sheet","worksheets":"worksheets"}
+				],
+	"Phrasal Verbs" : [{ "subject":"What are phrasal verbs","video":"Information Video","article":"Information sheet","worksheets":"worksheets"}
+				],
+	"Possessives" : [{ "subject":"What are possessives","video":"Information Video","article":"Information sheet","worksheets":"worksheets"}
+				],
+	"Prepositions" : [{ "subject":"What are prepositions?","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Prepositions of time","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Prepositions of place","video":"Information Video","article":"Information sheet","worksheets":"worksheets"}
+				],
+	"Pronouns" : [{ "subject":"What are pronouns?","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Indefinite Pronouns","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Personal Pronouns","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Possessive Pronouns","video":"Information Video","article":"Information sheet","worksheets":"worksheets"}
+				],
+	"Punctuation" : [{ "subject":"What is punctuation?","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Using capital letters","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Using full stops","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Using speech marks","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Using colons","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Using semi-colons","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Using question marks","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Using exclamation marks","video":"Information Video","article":"Information sheet","worksheets":"worksheets"}
+				],
+	"Subject, object and indirect object" : [{ "subject":"What is the grammatical subject","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"What is the grammatical object","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"What is the grammatical indirect object","video":"Information Video","article":"Information sheet","worksheets":"worksheets"}
+				],
+	"Tenses" : [{ "subject":"The English Tenses","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Simple present","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Simple past","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Simple future","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Present perfect","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Past perfect","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Future perfect","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"The continuous structure","video":"Information Video","article":"Information sheet","worksheets":"worksheets"}
+				],
+	"Verbs" : [{ "subject":"What are verbs","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Dynamic verbs and stative verbs","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Auxiliary verbs","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Modal verbs","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				{ "subject":"Irregular Verbs","video":"Information Video","article":"Information sheet","worksheets":"worksheets"}
+				],
+	"Word Class" : [{ "subject":"What is word class?","video":"Information Video","article":"Information sheet","worksheets":"worksheets"},
+				]
 };
